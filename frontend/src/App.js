90fd4b139e218d00e5ffdcdc46ce764965adff82
MJ Gomez-Saavedra <majogosaa98@gmail.com>
2023-12-03 23:35:32 -0500

    not authentic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { CelineSucc } from "./pages/CelineSucc.js";
+import { CelineFail } from "./pages/CelineFail.js";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/S-WNMHU" element={<CelineSucc />} />
+        <Route path="/F-WNMHU" element={<CelineFail />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-// import React from "react";
-
-// import "./App.css";
-// import { CelineSucc } from "./pages/CelineSucc.js";
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <CelineSucc></CelineSucc>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
